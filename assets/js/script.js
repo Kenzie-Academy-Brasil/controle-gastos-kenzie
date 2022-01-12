7bb624c845115add9452ddfe2f69ffe5745ff405
Maria Porcina <maria@kenzie.com.br>
2022-01-12 20:38:28 -0300

    atualizando

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// console.log('olá mundo');
+
+// Operadores básicos
+// + => operador de soma
+// = => operador de atribuição
+// += => soma e atribuição no mesmo operador
+// *= => multiplica e atribui
+// -= => subtrai e atribui
+
+// let minhaVariavel = "algum valor";
+
+let numero1 = 5;
+let numero2 = 20;
+
+let someNumeros = numero1 + numero2;
+
+console.log(someNumeros);
+
+let contador = 1;
+contador += 10;
+console.log(contador);
+
+// contador += 10;
+// console.log(contador);
+
+// contador += 10;
+// console.log(contador);
+
+// contador += 10;
+// console.log(contador);
+
+// contador += 10;
+
+// contador *= 5;
+// console.log(contador);
